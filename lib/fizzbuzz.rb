eaf53d2153acae53b5baf5238ee1b7d50e18ea5a
Shirley Macfarlane <shirley.macfarlane@waitrose.co.uk>
2022-01-07 15:32:39 +0000

    Updated with test and code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,10 @@
 def fizzbuzz(number)
-    'fizz'
+    return "fizzbuzz" if (number % 5 == 0 && number % 3 == 0)
+    if number % 5 == 0
+      return "buzz"
+    elsif number % 3 == 0
+      return "fizz"
+    else
+      return number
+    end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,3 +4,28 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
 end
+describe 'fizzbuzz' do 
+    it 'returns "buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+end
+describe 'fizzbuzz' do 
+    it 'returns 1 when passed 1' do
+        expect(fizzbuzz(1)).to eq 1
+    end
+end
+describe 'fizzbuzz' do 
+    it 'returns "fizzbuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+end
+describe 'fizzbuzz' do 
+    it 'returns "a" when passed a' do
+        expect(fizzbuzz("a")).to eq "a"
+    end
+end
+describe 'fizzbuzz' do 
+    it 'returns "-1" when passed -1' do
+        expect(fizzbuzz(-1)).to eq -1
+    end
+end
